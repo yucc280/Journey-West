a8d6f1e7e1109d6c609eeef827e00768db80228a
yucc280 <yuchan28033@gmail.com>
2026-07-23 14:02:51 +0800

    适配github pages部署：修改vite base路径+HashRouter路由

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AnalysisProvider } from './context/AnalysisContext';
 import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AnalysisProvider>
         <App />
       </AnalysisProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
